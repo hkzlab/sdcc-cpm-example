04d48ed4f425c95495344ad84eb982d875f3e362
Fabio Battaglia <hkzlabnet@gmail.com>
2014-01-07 19:46:27 +0100

    Add more ansi commands

--- src/syslib/ansi_term.c
+++ src/syslib/ansi_term.c
@@ -3,10 +3,13 @@
 #include "cbm_sysfunc.h"
 
 #define ESC_CHR 0x1B
-#define VT100_ANSI_CMD "\x1B<" // Sets VT100 and compatibles to ANSI mode 
+#define VT100_ANSI_CMD "<" // Sets VT100 and compatibles to ANSI mode 
 
-#define ANSI_CLRSCRN "\x1B[2J" // Erase entire screen
-#define ANSI_CLRTOEND "\x1B[J" // Erase from cursor to end of screen
+#define ANSI_CLRSCRN	"[2J"	// Erase entire screen
+#define ANSI_CLRREMSCRN	"[J"	// Erase from cursor to end of screen
+#define ANSI_CLRREMLNE	"[K"	// Erase from cursor to end of line
+#define ANSI_CLRBEGLNE	"[1K"	// Erase from beginning of line to cursor
+#define ANSI_CRLNE		"[2K"	// Erase line containing cursor
 
 
 void term_sendCommand(char *cmd);
@@ -17,6 +20,8 @@ void term_ANSIMode(void) {
 
 void term_sendCommand(char *cmd) {
 	int idx = 0;
+
+	cbm_putchar(ESC_CHR);
 	while (cmd[idx] != '\0')
 		cbm_putchar(cmd[idx]);
 }
